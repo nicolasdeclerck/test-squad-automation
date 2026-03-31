f49ea72170f346a765280e6b8bfda72a1c177bfc
Claude Worker <claude-worker@squad-automation.fr>
2026-03-31 16:59:56 +0000

    feat: close #11 - Mise en place d'un design system sobre en noir et blanc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
